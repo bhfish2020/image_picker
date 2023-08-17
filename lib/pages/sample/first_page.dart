04e01caf735e56d39ff5e277f2de44caae4e3b2c
Bhfish2020 <bhfish2020@gmail.com>
2023-08-17 08:08:54 -0700

    triggering build

--- lib/pages/sample/first_page.dart
+++ lib/pages/sample/first_page.dart
@@ -237,7 +237,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text('Initial Screen'),
       ),
       body: Center(
         child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
